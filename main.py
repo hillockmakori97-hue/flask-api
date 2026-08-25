6fd96dd02e26a51aed0157384ff7b4fd269491c0
hillockmakori97-hue <hillockmakori97@gmail.com>
2026-08-25 21:36:07 +0300

    created sql session

--- main.py
+++ main.py
@@ -3,9 +3,22 @@
 # It has to have a method (POST/GET/PUT/DELETE)
 # It has to have a status code
 # It has to return data as JSON
+from sqlalchemy import create_engine,select
 from flask import Flask,request,jsonify
-import json
+from models import Base,Product
+from sqlalchemy.orm import Session
+
 app=Flask(__name__)
+
+# create a database using sqlachemy engine
+engine = create_engine("sqlite:///./flask_duka_api.db", echo=True)
+
+#  create tables into the database using sqlalchemy
+Base.metadata.create_all(engine)
+
+# Create a session to do sql transaction 
+session=Session(engine)
+
 @app.route('/')
 def home():
     if request.method == 'GET':
@@ -15,5 +28,38 @@ def home():
     else:
         error={'Error': 'Method not allowed '}
         return jsonify(error),403
-    
+
+@app.route('/products')
+def products():
+    if request.method=='GET':
+    # fetch data from the database
+        query=select(Product)
+        products=session.scalars(query)
+        results=[]
+        for prod in products:
+            p={
+                'id':prod.id,
+                'user_id':prod.user_id,
+                'product_name':prod.product_name,
+                'buying_price':prod.buying_price
+            }
+            results=results.append(p)
+        return jsonify(results),200
+
+    elif request.method=='POST':
+
+        data=request.get_json()
+        if data['product_name']=='' or data['buying_price'] =='' or data['selling_price']=='':
+            error={'Error':'Ensure all fields are set'}
+            return jsonify(error),403
+        else:
+            # store in the database
+            pass
+
+
+    else:
+        error={'Error':'Method not allowed'}
+        return jsonify(error),405
+
+
 app.run(debug=True)
--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy import String,Integer,Float
+from sqlalchemy import String,Integer,Float,Date
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from datetime import date
+
 
 class Base(DeclarativeBase):
      pass
@@ -29,17 +31,24 @@ class Sale(Base):
 
     id:Mapped[int]=mapped_column(Integer,primary_key=True)
     user_id:Mapped[int]=mapped_column(Integer,ForeignKey('users.id'))
+    date_sold:Mapped[date]=mapped_column(Date)
 
 class Payment(Base):
     __tablename__='payments'
     id:Mapped[int]=mapped_column(Integer,primary_key=True)
     sale_id:Mapped[int]=mapped_column(Integer,ForeignKey('sales.id'))
+    date_paid:Mapped[date]=mapped_column(Date)
 class Purchase(Base):
     __tablename__='purchases'
     id:Mapped[int]=mapped_column(Integer,primary_key=True)
     product_id:Mapped[int]=mapped_column(Integer,ForeignKey('products.id'))
-class sale_detail(Base):
+    date_purchased:Mapped[date]=mapped_column(Date)
+    purchase_price :  Mapped[Float] = mapped_column(Float)
+    
+class Sale_detail(Base):
     __tablename__='sales_details'
     id:Mapped[int]=mapped_column(Integer,primary_key=True)
     product_id:Mapped[int]=mapped_column(Integer,ForeignKey('products.id'))
     sale_id:Mapped[int]=mapped_column(Integer,ForeignKey('sales.id'))
+    quantity:Mapped[float]=mapped_column(Float)
+    amount:Mapped[int]=mapped_column(Integer)
